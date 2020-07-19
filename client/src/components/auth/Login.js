7b24fde34cbc980cdd14106ce640eeef0777b22d
IgnatDubovskiy <57371070+IgnatDubovskiy@users.noreply.github.com>
2020-07-19 14:38:39 +0300

    Connecting redux, creating store, creating an Alert component with own reducer and AC

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const {email, password} = formData
+
+  const inputChangeHandler = (e) => setFormData({
+    ...formData,
+    [e.target.name]: e.target.value
+  })
+
+  const formSubmitHandler = async (e) => {
+    e.preventDefault()
+    console.log('SUCCESS')
+  }
+
   return (
-    <div>
-      Login
+    <div className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user" /> Sign Into Your Account</p>
+      <form className="form" action="create-profile.html" onSubmit={formSubmitHandler}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={inputChangeHandler}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login"/>
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
     </div>
   )
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { setAlert } from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+const Register = ({ setAlert }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = formData
+
+  const inputChangeHandler = (e) => setFormData({
+    ...formData,
+    [e.target.name]: e.target.value
+  })
+
+  const formSubmitHandler = async (e) => {
+    e.preventDefault()
+    if (password !== password2) {
+      setAlert('Passwords do not match', 'danger')
+    } else {
+      console.log('SUCCESS')
+    }
+  }
+
   return (
-    <div>
-      Register
+    <div className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user" /> Create Your Account</p>
+      <form className="form" action="create-profile.html" onSubmit={formSubmitHandler}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={inputChangeHandler}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={inputChangeHandler}
+            required
+          />
+          <small className="form-text"
+          >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            value={password2}
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register"/>
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
     </div>
   )
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+
+export default connect(
+  null,
+  {setAlert}
+  )(Register)
